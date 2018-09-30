1010667feba8c1e08ee77f5fa70db2bc259e0651
PaulRobson85 <Paul.Robson@carfinance247.co.uk>
2018-09-30 13:22:31 +0100

    initial commit

---
+++ Assets/Scripts/DiceRollController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DiceRollController : MonoBehaviour {
+
+    public List<GameObject> DiceList;
+    public GameObject Dice;
+
+    public int DiceCount;
+
+	// Use this for initialization
+	void Start ()
+    {
+        var dicePosition = new Vector3(0, 5, 0);
+        var diceRotation = new Quaternion(5, 5, 5, 5);
+        Dice.transform.position = dicePosition;
+        
+        for (int i=0; i<DiceCount; i++)
+        {
+            //var diceRigidBody = Dice.GetComponent<Rigidbody>();
+
+            DiceList.Add(Instantiate(Dice, dicePosition, new Quaternion()));
+        }
+	}
+}
